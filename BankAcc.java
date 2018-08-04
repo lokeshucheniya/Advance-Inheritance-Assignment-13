cdcaa252701b8d9123659ffb1341a8f23dc2a18e
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 23:21:19 +0530

    Update BankAcc.java

--- BankAcc.java
+++ BankAcc.java
@@ -4,12 +4,14 @@ public  abstract class BankAcc {
 	private int accNo;
 	private String accNm;
 	private float accBal;
+	
 	//Parameterized constructor 
 	public BankAcc(int accNo, String accNm, float accBal) {
 		this.accNo = accNo;
 		this.accNm = accNm;
 		this.accBal = accBal;
 	}
+	
 	//Getters and setters for instance members
 	public int getAccNo() {
 		return accNo;
@@ -35,14 +37,14 @@ public  abstract class BankAcc {
 		this.accBal = accBal;
 	}
 	
-	public abstract void withdraw(float amount); //Abstract withdraw method
+	public abstract void withdraw(float amount); 				//Abstract withdraw method
 	
-	public void deposit(float amount) {  //Deposit amount
+	public void deposit(float amount) {  					//Deposit amount
 		this.accBal+=amount;
 	}
 
 	@Override
-	public String toString() {  //Display account details
+	public String toString() {  						//Display account details
 		return "BankAcc [Account number=" + accNo+ ", Account holder name=" + accNm + ", Account balance=" + accBal + "]";
 	}
 }
