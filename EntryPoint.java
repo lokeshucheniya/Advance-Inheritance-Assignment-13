c381358d259f3b5edecd1038a44abfe81c092fe1
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 23:30:23 +0530

    Update EntryPoint.java

--- EntryPoint.java
+++ EntryPoint.java
@@ -1,13 +1,14 @@
-//Entry point to test application
+
 public class EntryPoint {
 	public static void main(String[] args) {
 		
 		BankFactory mmBankFactory=new MMBankFactory();
 		
 		//Open a new salaried savings account
-		SavingAcc mmSavingAcc=mmBankFactory.getNewSavingAcc(100, "Sayali", 5000, true); 
+		SavingAcc mmSavingAcc=mmBankFactory.getNewSavingAcc(145, "Sample1", 5000, true); 
+		
 		//Open a new current account
-		CurrentAcc mmCurrentAcc=mmBankFactory.getNewCurrentAcc(101, "Siddhi", 3000, 10000);
+		CurrentAcc mmCurrentAcc=mmBankFactory.getNewCurrentAcc(189, "Sample2", 3000, 10120);
 		
 		mmSavingAcc.withdraw(500);
 		System.out.println(mmSavingAcc);
@@ -22,7 +23,7 @@ public class EntryPoint {
 		System.out.println(mmCurrentAcc);
 		
 		//Open a new non-salaried savings account
-		SavingAcc mmSavingAcc2=mmBankFactory.getNewSavingAcc(102, "Sayali", 5000, false); 
+		SavingAcc mmSavingAcc2=mmBankFactory.getNewSavingAcc(189, "Sample2", 5000, false); 
 		mmSavingAcc2.withdraw(4500);
 		System.out.println(mmSavingAcc2);
 	}
